ba6decccdee658090893385a7d44121b852468f8
Madan Mariani <marianim@student.douglascollege.ca>
2020-05-19 17:47:46 -0700

    Create the first class named myAppTester

---
+++ src/myJavaProject/myAppTester.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package myJavaProject;
+
+/**
+ * @author cheng
+ *
+ */
+public class myAppTester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
