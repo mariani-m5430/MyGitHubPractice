d0eec5c82642a9a7af176c8914bb09e83559514b
Madan Mariani <marianim@student.douglascollege.ca>
2020-05-19 17:59:49 -0700

    Write program to input phone number and display it

--- src/myJavaProject/myAppTester.java
+++ src/myJavaProject/myAppTester.java
@@ -3,6 +3,8 @@
  */
 package myJavaProject;
 
+import java.util.Scanner;
+
 /**
  * @author cheng
  *
@@ -13,7 +15,30 @@ public class myAppTester {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello Software Engineering World");
+		int a=0;
+		StringBuilder phonenum;
+		StringBuilder quit = new StringBuilder("999");		
+		do {
+			Scanner sc = new Scanner(System.in);
+			System.out.print("Please input 10 digit (or input 999 to quit) >>");
+			phonenum = new StringBuilder(sc.nextLine());
+			for(int i = 0 ; i<phonenum.length(); ++i) {
+				if(!(Character.isDigit(phonenum.charAt(i)))){
+					a=1; 
+					i=phonenum.length();}	
+			}
+
+			if(phonenum.toString().equals(quit.toString()))
+				System.out.println("Thank you for using this program!");
+			else if (a==1 || phonenum.length()!=10) {
+				System.out.println("The number you entered is not 10 digit");
+				a=0;
+				}
+			    else 
+			    	System.out.println("the phone number is (" + phonenum.substring(0, 3) + ")" 
+			               + phonenum.substring(3, 6) + "-" + phonenum.substring(6, 10));
+		} while(phonenum.compareTo(quit)!=0);
+
 
 	}
 
